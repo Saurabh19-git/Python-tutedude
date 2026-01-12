bb272e0fd05f186dde5dcce7510b731ee16a26b4
Saurabh19-git <msaurabh726@gmail.com>
2026-01-13 03:49:35 +0530

    Remove 'Mathematical_Operations.py Personalized_Greeting.py' files

--- Personalized_Greeting.py
+++
@@ -1,6 +0,0 @@
-First_Name = input("Enter your first name: ")
-Last_Name = input("Enter your last name: ")
-Full_name = First_Name+" "+Last_Name
-
-print(f"Hello, {First_Name} {Last_Name}! Welcome to the Python program.")
-# print("Hello, "+Full_name+"! Welcome to the Python program.")
